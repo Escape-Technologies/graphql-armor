29e3ec3038c57ff2df27103121360863e24e4f0b
c3b5aw <dev@c3b5aw.dev>
2022-07-28 11:55:21 +0200

    feat: add linters and workflows

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  "env": {
+      "es2021": true,
+      "node": true
+  },
+  "extends": [
+      "google"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+  },
+  "plugins": [
+      "@typescript-eslint"
+  ],
+  "rules": {
+      "object-curly-spacing": "off",
+  },
+  "ignorePatterns": [
+      "**/node_modules/**",
+      "**/dist/**"
+  ]
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# GraphQL-Armor [![CI](https://github.com/Escape-Technologies/graphql-armor/actions/workflows/ci.yaml/badge.svg)](https://github.com/Escape-Technologies/graphql-armor/actions/workflows/ci.yaml)

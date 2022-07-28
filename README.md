8b75afec6208451b9efdffcc3dae53a3749c275e
c3b5aw <dev@c3b5aw.dev>
2022-07-28 17:12:55 +0200

    chore: update lint/configuration flow
    Co-authored-by: thibaud.labat@hotmail.fr <thibaud.labat@hotmail.fr>

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # GraphQL-Armor [![CI](https://github.com/Escape-Technologies/graphql-armor/actions/workflows/ci.yaml/badge.svg)](https://github.com/Escape-Technologies/graphql-armor/actions/workflows/ci.yaml)
+
+## Usage
+
+```typescript
+import { Armor } from '../src';
+const armor = new Armor({});
+
+const server = armor.apolloServer({
+  typeDefs,
+  resolvers,
+  cache: 'bounded',
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+```
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,25 +1,29 @@
 const Configuration = {
   /*
-  * Resolve and load @commitlint/config-conventional from node_modules.
-  * Referenced packages must be installed
-  */
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
   extends: ['@commitlint/config-angular'],
   /*
-  * Resolve and load @commitlint/format from node_modules.
-  * Referenced package must be installed
-  */
+   * Resolve and load @commitlint/format from node_modules.
+   * Referenced package must be installed
+   */
   formatter: '@commitlint/format',
   /*
-  * Whether commitlint uses the default ignore rules.
-  */
+   * Whether commitlint uses the default ignore rules.
+   */
   defaultIgnores: true,
   /*
-  * Custom URL to show upon failure
-  */
+   * Custom URL to show upon failure
+   */
   helpUrl:
     'https://github.com/angular/angular/blob/22b96b9/CONTRIBUTING.md#-commit-message-guidelines',
   rules: {
-    'type-enum': [2, 'always', ['ci', 'docs', 'feat', 'fix', 'refactor', 'test', 'chore']],
+    'type-enum': [
+      2,
+      'always',
+      ['ci', 'docs', 'feat', 'fix', 'refactor', 'test', 'chore'],
+    ],
   },
 };
 

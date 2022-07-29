30c958d4295a7af4a73ca709eb2547d7468539d9
c3b5aw <dev@c3b5aw.dev>
2022-07-29 12:49:47 +0200

    refactor: introspection as a plugin

--- src/plugins/Introspection.ts
+++ src/plugins/Introspection.ts
@@ -1,32 +1,44 @@
-import {
-  ValidationContext,
-  FieldNode,
-  ASTVisitor,
-  GraphQLError,
-} from 'graphql';
 import { ArmorPlugin } from '../ArmorPlugin';
-import { ValidationRule, PluginConfig } from '../types';
+import { PluginConfig, PluginDefinition } from '../types';
 
 export type IntrospectionConfig = { Introspection?: PluginConfig };
 export const DefaultIntrospectionConfig = {
   _namespace: 'Introspection',
   enabled: false,
+  options: {
+    headersWhitelist: {
+      'x-allow-introspection': 'allow',
+      ...(process.env.ESCAPE_IDENTIFIER
+        ? { 'x-escape-identifier': process.env.ESCAPE_IDENTIFIER }
+        : {}),
+    },
+  },
 };
 
-// ToDo: Whitelist headers pairs -> Maybe use a apollo plugin instead?
-function __rule(context: ValidationContext): ASTVisitor {
+const plugin = ({
+  options: { headersWhitelist },
+}: PluginConfig): PluginDefinition => {
+  const whitelist = headersWhitelist.map((header) => header.toLowerCase());
+
   return {
-    Field(node: FieldNode) {
-      const blacklist = ['__schema', '__type'];
-      if (blacklist.includes(node.name.value)) {
-        context.reportError(new GraphQLError('Introspection is disabled'));
+    async requestDidStart({ request }) {
+      if (request.query!.includes('__schema')) {
+        const headers = request.http!.headers;
+
+        const whitelistedHeaders = whitelist.filter((header) =>
+          headers.has(header)
+        );
+
+        if (whitelistedHeaders.length === 0) {
+          throw new Error('Introspection is disabled');
+        }
       }
     },
   };
-}
+};
 
 export class Introspection extends ArmorPlugin {
-  getValidationRules(): ValidationRule[] {
-    return [__rule];
+  getApolloPlugins(): PluginDefinition[] {
+    return [plugin(this.getConfig())];
   }
 }

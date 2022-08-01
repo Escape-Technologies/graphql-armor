46097786abd1a081c0e2310e7194b53d74cf0331
c3b5aw <dev@c3b5aw.dev>
2022-08-01 12:42:02 +0200

    fix: use nest cli to start example

--- examples/nestjs/src/main.ts
+++ examples/nestjs/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000;
+  console.log(`Starting server on port ${port}`);
+  await app.listen(port);
 }
 bootstrap();

7abaca0e7cba67d7e01d919ac5ba61f9dcc3fc01
Maxence Lecanu <lecanu.maxence@gmail.com>
2022-08-01 15:32:34 +0200

    feat(repo): move the repo under a real monorepo structure (#13)
    * feat(repo): move the repo under a real monorepo structure
    
    * fix: remove yarn cache
    
    * fix(repo): fix ci
    
    * fix: remove profiler & update deps
    
    * feat(repo): move the repo under a real monorepo structure
    
    * fix: remove yarn cache
    
    * fix: remove profiler & update deps
    
    * fix(repo): fix ci
    
    * fix(repo): fix ci
    
    * fix(repo): fix pre-commit hook
    
    Co-authored-by: Maxence Lecanu <maxence@escape.tech>
    Co-authored-by: c3b5aw <dev@c3b5aw.dev>

---
+++ packages/graphql-armor/src/plugins/Introspection.ts
@@ -0,0 +1,43 @@
+import { ArmorPlugin } from '../ArmorPlugin';
+import { PluginConfig, PluginDefinition } from '../types';
+
+export type BlockIntrospectionConfig = { BlockIntrospection?: PluginConfig };
+export const DefaultBlockIntrospectionConfig = {
+  _namespace: 'BlockIntrospection',
+  enabled: true,
+  options: {
+    headersWhitelist: {
+      'x-allow-introspection': 'allow',
+      ...(process.env.ESCAPE_IDENTIFIER ? { 'x-escape-identifier': process.env.ESCAPE_IDENTIFIER } : {}),
+    },
+  },
+};
+
+const plugin = ({ options: { headersWhitelist } }: PluginConfig): PluginDefinition => {
+  return {
+    async requestDidStart({ request }) {
+      if (request.query!.includes('__schema')) {
+        const headers = request.http!.headers;
+
+        let containsWhitelistedHeader = false;
+        for (const key in headersWhitelist) {
+          if (headers.has(key)) {
+            if (headersWhitelist[key] === headers.get(key)) {
+              containsWhitelistedHeader = true;
+            }
+          }
+        }
+
+        if (!containsWhitelistedHeader) {
+          throw new Error('Introspection is disabled');
+        }
+      }
+    },
+  };
+};
+
+export class BlockIntrospection extends ArmorPlugin {
+  getApolloPlugins(): PluginDefinition[] {
+    return [plugin(this.getConfig())];
+  }
+}

fb7461ad23c994d24d2e3ac2a26c5368a507d9f8
c3b5aw <dev@c3b5aw.dev>
2022-08-01 12:58:11 +0200

    fix: update books types

--- examples/nestjs/src/book/book.resolver.ts
+++ examples/nestjs/src/book/book.resolver.ts
@@ -2,18 +2,20 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 
 import { BookService } from './book.service';
 import { Book } from './book.model';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 @Resolver('Book')
 export class BookResolver {
   constructor(private readonly booksService: BookService) {}
 
-  @Query('books')
-  public async findAll(): Promise<Book[]> {
+  @Query(() => [Book], { nullable: true })
+  public async findAll() {
     return this.booksService.findAll();
   }
 
-  @Query('book')
-  public async findOne(@Args('id') id: string): Promise<Book | undefined> {
+  @Query(() => Book)
+  public async findOne(@Args('id') id: string) {
     return this.booksService.findById(id);
   }
 }

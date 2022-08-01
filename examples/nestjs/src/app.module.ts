531b20aa9e229eaed88555a7f47bace91afc89a6
c3b5aw <dev@c3b5aw.dev>
2022-08-01 12:51:08 +0200

    fix: missing pkg

--- examples/nestjs/src/app.module.ts
+++ examples/nestjs/src/app.module.ts
@@ -3,20 +3,17 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
-// import { ConferenceModule } from './conference/conference.module';
-// import { SessionModule } from './session/session.module';
-// import { SpeakerModule } from './speaker/speaker.module';
+import { ApolloDriver } from '@nestjs/apollo';
 
 console.log(process.env.ENGINE_API_KEY);
 @Module({
   imports: [
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
-      installSubscriptionHandlers: true,
-      engine: {
-        apiKey: process.env.ENGINE_API_KEY,
-      },
       context: ({ req }) => ({ req }),
+      playground: true,
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
     }),
     BookModule,
   ],

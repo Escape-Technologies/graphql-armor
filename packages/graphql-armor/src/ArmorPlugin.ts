a2dee8ab000fd1a422821989a50ef1005aae810a
c3b5aw <dev@c3b5aw.dev>
2022-08-02 17:14:28 +0200

    wip

--- packages/graphql-armor/src/ArmorPlugin.ts
+++ packages/graphql-armor/src/ArmorPlugin.ts
@@ -1,13 +1,20 @@
 import { PluginConfig } from './types';
-import { ApolloArmor } from './index';
 import { ValidationRule } from 'graphql';
 import { PluginDefinition } from 'apollo-server-core/src/types';
+import { Plugin } from '@envelop/core';
 
 export class ArmorPlugin {
   private readonly _config: PluginConfig;
+  private readonly _logger: null|((message:string)=>void);
 
-  constructor(config: PluginConfig) {
+  constructor(config: PluginConfig, logger:null|((message:string)=>void) = null) {
     this._config = config;
+    this._logger = logger;
+  }
+
+  log(message:string){
+    if(this._logger)
+      this._logger(message);
   }
 
   getConfig(): PluginConfig {
@@ -15,6 +22,10 @@ export class ArmorPlugin {
   }
 
   getApolloPlugins(): PluginDefinition[] {
+    return [];
+  }
+
+  getEnvelopPlugins(): Plugin[] {
     return [];
   }
 

194d3c94ec82c5dbc13562d2b58908d739ab7888
c3b5aw <dev@c3b5aw.dev>
2022-08-02 11:45:01 +0200

    refactor(api): remove plugin state

--- packages/graphql-armor/src/types.ts
+++ packages/graphql-armor/src/types.ts
@@ -3,15 +3,6 @@ import { CharacterLimitConfig } from './plugins/CharacterLimit';
 import { CostAnalysisConfig } from './plugins/CostAnalysis';
 import { BlockIntrospectionConfig } from './plugins/BlockIntrospection';
 
-// Plugin Event
-export enum PluginState {
-  ENABLED = 'enabled',
-  DISABLED = 'disabled',
-  REGISTERED = 'registered',
-  UNREGISTERED = 'unregistered',
-}
-export type PluginUpdateEvent = (status: PluginState, plugin: PluginConfig) => void;
-
 // Config
 export type PluginConfig = {
   _namespace?: string;

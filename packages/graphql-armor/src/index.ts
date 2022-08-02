27885ce3c76154ed682eb9a805c25039082fc95b
c3b5aw <dev@c3b5aw.dev>
2022-08-02 11:46:32 +0200

    feat(examples): add full apollo example as a package (#21)
    * feat(examples): add full apollo example as a package
    
    * refactor(api): remove plugin state

--- packages/graphql-armor/src/index.ts
+++ packages/graphql-armor/src/index.ts
@@ -4,7 +4,7 @@ import { ValidationContext } from 'graphql';
 import * as Plugins from './plugins/';
 
 import { ArmorPlugin } from './ArmorPlugin';
-import { GraphQLArmorConfig, PluginUpdateEvent, PluginState } from './types';
+import { GraphQLArmorConfig } from './types';
 import { ConfigService } from './config';
 
 /**
@@ -13,15 +13,10 @@ import { ConfigService } from './config';
  * This will inject remediations into the config.
  * @param apolloConfig The ApolloConfig object
  * @param armorConfig  The GraphQLArmorConfig object
- * @param onPluginUpdate  The function to call when a plugin is updated
  * @returns The configuration object with the remediation injected
  */
-function ArmorApolloConfig<T>(
-  apolloConfig: ApolloConfig<T>,
-  armorConfig?: GraphQLArmorConfig,
-  onPluginUpdate?: PluginUpdateEvent,
-): ApolloConfig<T> {
-  const service = new ApolloArmor(armorConfig, onPluginUpdate);
+function ArmorApolloConfig<T>(apolloConfig: ApolloConfig<T>, armorConfig?: GraphQLArmorConfig): ApolloConfig<T> {
+  const service = new ApolloArmor(armorConfig);
   return service.getConfig(apolloConfig);
 }
 
@@ -42,25 +37,15 @@ function ArmorApolloConfigU(config: any): any {
 class ApolloArmor {
   private readonly _plugins: ArmorPlugin[] = [];
   private readonly _configService: ConfigService;
-  private readonly _onPluginUpdate?: PluginUpdateEvent;
 
-  /*
-   * Push each plugin to the plugins array
-   */
-  constructor(config?: GraphQLArmorConfig, onPluginUpdate?: PluginUpdateEvent) {
+  constructor(config?: GraphQLArmorConfig) {
     this._configService = new ConfigService(config);
-    this._onPluginUpdate = onPluginUpdate;
 
     for (const plugin of Object.values(Plugins)) {
       const pluginConfig = this._configService.getPluginConfig(plugin.name);
 
       if (pluginConfig.enabled) {
         this._plugins.push(new plugin(this, pluginConfig));
-
-        // Spread the plugin registration event
-        if (this._onPluginUpdate) {
-          this._onPluginUpdate(PluginState.REGISTERED, pluginConfig);
-        }
       }
     }
   }

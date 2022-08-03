4e7e0d6105c6899cf32a155bbf9a11109ff83b1b
thibaudlabat <thibaudlabat@gitlab.com>
2022-08-03 17:59:35 +0200

    WIP : complexity

--- packages/graphql-armor/src/apollo/protections/cost-analysis.ts
+++ packages/graphql-armor/src/apollo/protections/cost-analysis.ts
@@ -1,66 +1,39 @@
-import { ApolloError } from 'apollo-server-core';
+import { ApolloError} from 'apollo-server-core';
 import { ValidationContext } from 'graphql';
 
 import { ApolloServerConfigurationEnhancement, ApolloProtection } from './base-protection';
 
-import QueryComplexity, {
-  ComplexityEstimator,
-  ComplexityEstimatorArgs,
-} from '../../../lib/graphql-query-complexity/QueryComplexity';
-import { CostAnalysisOptions } from '../../config';
-
-function simpleEstimator(options?: { defaultComplexity?: number }): ComplexityEstimator {
-  const defaultComplexity = options && typeof options.defaultComplexity === 'number' ? options.defaultComplexity : 1;
-  return (args: ComplexityEstimatorArgs): number | void => {
-    return defaultComplexity + args.childComplexity;
-  };
-}
+import QueryComplexity2 from "../../lib/graphql-query-complexity/QueryComplexity2";
 
 const validationRule =
-  (options: CostAnalysisOptions) =>
-  (context: ValidationContext): QueryComplexity =>
-    new QueryComplexity(
-      context,
-      {
-        maxDepth: options.maxDepth,
-        maximumComplexity: options.maxCost,
-        maxAlias: options.maxAlias,
-        maxDirectives: options.maxDirectives,
-
-        variables: {},
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        onComplete: () => {},
-        createError: () => {
-          // this.log(`Query is too complex: ${actual}. Maximum allowed complexity: ${max}`);
-          // we don't want people to know how complexity is computed
-          // return new GraphQLError(`Query is too complex: ${actual}. Maximum allowed complexity: ${max}`);
-          return new ApolloError(`Query is too complex.`, 'BAD_USER_INPUT');
-        },
-        estimators: [
-          simpleEstimator({
-            defaultComplexity: options.defaultComplexity,
-          }),
-        ],
-      },
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      () => {},
-      // (message) => this.log(message),
-    );
+    (options: CostAnalysisOptions) =>
+        (context: ValidationContext) =>   new QueryComplexity2(
+            context,
+            options,
+            (message:string) => {  throw new ApolloError(message,"BAD_USER_INPUT");  }
+        );
 
 export class ApolloCostAnalysisProtection extends ApolloProtection {
   get isEnabled(): boolean {
-    // default
-    if (!this.config.costAnalysis) return true;
-    return this.config.costAnalysis.enabled;
+    // TODO: change
+      return true;
+      // default
+    //if (!this.config.costAnalysis) return true;
+    //return this.config.costAnalysis.enabled;
   }
 
   get options(): CostAnalysisOptions {
     return {
       maxCost: this.config.costAnalysis?.options?.maxCost || 5000,
-      defaultComplexity: this.config.costAnalysis?.options?.defaultComplexity || 1,
       maxDepth: this.config.costAnalysis?.options?.maxDepth || 6,
       maxAlias: this.config.costAnalysis?.options?.maxAlias || 15,
       maxDirectives: this.config.costAnalysis?.options?.maxDirectives || 50,
+
+        // TODO : what would be the best default values ?
+        // best idea would be to run a benchmark on some API and compare with real measured time
+        objectCost: this.config.costAnalysis?.options?.objectCost || 2,
+        scalarCost: this.config.costAnalysis?.options?.scalarCost || 1,
+        depthCostFactor: this.config.costAnalysis?.options?.depthCostFactor || 1.5,
     };
   }
 
--- packages/graphql-armor/src/envelop/protections/protection-options.ts
+++ packages/graphql-armor/src/envelop/protections/protection-options.ts
@@ -3,11 +3,15 @@ export type BlockFieldSuggestionOptions = undefined;
 export type CharacterLimitOptions = { maxLength: number };
 
 export type CostAnalysisOptions = {
+
   maxCost: number;
-  defaultComplexity: number;
   maxDepth: number;
   maxAlias: number;
   maxDirectives: number;
+
+  objectCost: number;
+  scalarCost: number;
+  depthCostFactor: number;
 };
 
 export type ProtectionOptions = BlockFieldSuggestionOptions | CharacterLimitOptions | CostAnalysisOptions;

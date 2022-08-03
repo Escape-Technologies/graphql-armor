1a9aa70851c688a2a03750da9bd875bd62c67af5
c3b5aw <dev@c3b5aw.dev>
2022-08-03 19:21:03 +0200

    Feat/yoga field suggestion (#35)
    * chore(examples): yoga books resolver
    * fix(yoga): field suggestion
    * fix(yoga): typing
    * feat(debugger): more example added

--- examples/yoga/src/main.ts
+++ examples/yoga/src/main.ts
@@ -1,12 +1,15 @@
 import { createServer } from '@graphql-yoga/node'
 import { schema } from './schema'
+import { EnvelopArmor} from '@escape.tech/graphql-armor'
 
-import {EnvelopArmor} from '@escape.tech/graphql-armor'
 const armor = new EnvelopArmor()
 const enhancements = armor.protect()
 
 async function main() {
-  const server = createServer({ schema, plugins: [...enhancements.plugins] })
+  const server = createServer({
+    schema,
+    plugins: [...enhancements.plugins],
+  })
   await server.start()
 }
 
--- packages/graphql-armor/src/envelop/protections/block-field-suggestion.ts
+++ packages/graphql-armor/src/envelop/protections/block-field-suggestion.ts
@@ -1,27 +1,24 @@
-import { useErrorHandler } from '@envelop/core';
+import { useMaskedErrors } from '@envelop/core';
+import { GraphQLError } from 'graphql';
 import { EnvelopConfigurationEnhancement, EnvelopProtection } from './base-protection';
-
-// @TODO: fix this
-const plugin = useErrorHandler((errors) => {
-  console.log(errors);
-  for (const error of errors) {
-    console.log(error.message);
-    error.message = error.message.replace(/Did you mean ".+"/g, '[Suggestion message hidden by GraphQLArmor]');
-  }
-  console.log(errors);
-  return errors;
-});
 
 export class EnvelopBlockFieldSuggestionProtection extends EnvelopProtection {
   get isEnabled(): boolean {
-    // default
     if (!this.config.blockFieldSuggestion) return true;
     return this.config.blockFieldSuggestion.enabled;
   }
 
   protect(): EnvelopConfigurationEnhancement {
     return {
-      plugins: [plugin],
+      plugins: [
+        useMaskedErrors({
+          handleValidationErrors: true,
+          formatError: (error: GraphQLError, _message, _isDev) => {
+            error.message = error.message.replace(/Did you mean ".+"/g, '[Suggestion message hidden by GraphQLArmor]');
+            return error;
+          },
+        }),
+      ],
     };
   }
 }

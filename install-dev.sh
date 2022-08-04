dcbee56164df75a84fb4950c3784f99fd8b7996a
c3b5aw <pro@c3b5aw.dev>
2022-08-04 14:56:21 +0200

    chore: ci

--- install-dev.sh
+++ install-dev.sh
@@ -9,4 +9,4 @@ yarn dlx mookme init --only-hook --skip-types-selection
 echo "Building GraphQL Armor"
 yarn workspace @escape.tech/graphql-armor build
 
-echo "Run commands using `yarn workspace @escape.tech/pkg cmd`"
+echo "Run commands using `yarn workspace @escape.tech/{pkg} {cmd}`"

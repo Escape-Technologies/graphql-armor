ceeae5fb14f19666163764b1dff917aac9e3eca6
c3b5aw <pro@c3b5aw.dev>
2022-08-04 11:43:49 +0200

    chore(example): nestjsdefault port

--- examples/nestjs/src/main.ts
+++ examples/nestjs/src/main.ts
@@ -7,7 +7,7 @@ const logger = new Logger('Main');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
   logger.log(`Starting server on port ${port}`);
 
   await app.listen(port);

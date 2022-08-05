c3afa11d23164815d5fd12195385882d48431683
c3b5aw <pro@c3b5aw.dev>
2022-08-05 12:13:14 +0200

    fix(ci): update for individual distrib

--- install-dev.sh
+++ install-dev.sh
@@ -7,6 +7,6 @@ echo "Initializing mookme..."
 yarn dlx mookme init --only-hook --skip-types-selection
 
 echo "Building GraphQL Armor"
-yarn workspace @escape.tech/graphql-armor build
+yarn workspaces foreach -ptv run build
 
 echo "Run commands using `yarn workspace @escape.tech/{pkg} {cmd}`"

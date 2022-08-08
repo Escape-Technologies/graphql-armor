1b2a6136b0fc3df349c163f29158e6fcdb69b682
c3b5aw <pro@c3b5aw.dev>
2022-08-08 16:30:44 +0200

    wip

---
+++ examples/apollo-envelop/README.md
@@ -0,0 +1,7 @@
+# Apollo using envelop
+
+Example using [Apollo](https://www.apollographql.com/docs/apollo-server/) and envelop.
+
+```bash
+yarn workspace @escape.tech/graphql-armor-example-apollo-envelop cmd
+```
---
+++ examples/apollo-envelop/src/index.ts
@@ -0,0 +1,93 @@
+import { ApolloServer } from 'apollo-server';
+import { envelop, useSchema, useTiming } from '@envelop/core';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { EnvelopArmor } from '@escape.tech/graphql-armor';
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+
+const schema = makeExecutableSchema({
+  typeDefs: /* GraphQL */ `
+    type Query {
+      hello: String!
+      books: [Book]
+    }
+
+    type Book {
+      title: String
+      author: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      books: () => books,
+      hello: () => 'World',
+    },
+  },
+});
+
+
+const armor = new EnvelopArmor();
+const enhancements = armor.protect();
+
+const getEnveloped = envelop({
+  plugins: [
+    useSchema(schema),
+    useTiming(),
+    ...enhancements.plugins,
+    {
+      onValidate: () => {
+        console.log('on validate');
+      },
+      onParse: () => {
+        console.log('on parse');
+      },
+      onEnveloped: () => {
+        console.log('on enveloped');
+      },
+      onContextBuilding: () => {
+        console.log('on context building');
+      },
+      onExecute: () => {
+        console.log('on execute');
+      },
+      onPluginInit: () => {
+        console.log('on plugin initialize');
+      },
+      onResolverCalled: () => {
+        console.log('on resolver called');
+      },
+      onSchemaChange: () => {
+        console.log('on schema change');
+      }
+    },
+  ],
+});
+
+const server = new ApolloServer({
+  schema,
+  executor: async requestContext => {
+    const { schema, execute, contextFactory } = getEnveloped({ req: requestContext.request.http });
+
+    return execute({
+      schema,
+      document: requestContext.document,
+      contextValue: await contextFactory(),
+      variableValues: requestContext.request.variables,
+      operationName: requestContext.operationName,
+    });
+  },
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground({ endpoint: '/graphql' })],
+});
+
+server.listen(4000);

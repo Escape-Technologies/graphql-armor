f4c54d2128a8f155f30b953d6a48e17762306266
c3b5aw <pro@c3b5aw.dev>
2022-08-11 01:42:32 +0200

    refactor: apollo structure to be testable

--- examples/apollo/src/index.ts
+++ examples/apollo/src/index.ts
@@ -1,85 +1,10 @@
-import { ApolloArmor } from '@escape.tech/graphql-armor';
-import { gql } from 'apollo-server';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-import { ApolloServer } from 'apollo-server-express';
+import { server } from './server';
 
 const express = require('express');
-const http = require('http');
-
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Nested {
-    child: Nested
-    text: String
-  }
-
-  type Query {
-    books: [Book]
-    nested: Nested
-  }
-
-  type Mutation {
-    addBook(title: String, author: String): Book
-  }
-`;
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
-const nestedResolver = (i = 0) => {
-  return async () => {
-    await new Promise((resolve) => setTimeout(resolve, 50 * i));
-    return {
-      child: nestedResolver(i + 1),
-      text: 'text' + i,
-    };
-  };
-};
-
-const resolvers = {
-  Query: {
-    books: () => books,
-    nested: nestedResolver(0),
-  },
-  Mutation: {
-    addBook: (title: string, author: string) => {
-      return { title: 'title_test', author: 'author_test' };
-    },
-  },
-};
-
 const app = express();
 
+const http = require('http');
 const httpServer = http.createServer(app);
-
-const armor = new ApolloArmor({
-  characterLimit: {
-    enabled: true,
-    maxLength: 10000,
-  },
-});
-
-const enhancements = armor.protect();
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  cache: 'bounded',
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ...enhancements.plugins],
-  validationRules: [...enhancements.validationRules],
-});
 
 (async () => {
   await server.start();

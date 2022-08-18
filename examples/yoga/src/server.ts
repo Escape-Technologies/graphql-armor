ee7ead056e01957fa462db1bd3bcacd34a61fba7
c3b5aw <pro@c3b5aw.dev>
2022-08-18 16:28:55 +0200

    fix: merge precision

--- examples/yoga/src/server.ts
+++ examples/yoga/src/server.ts
@@ -3,36 +3,35 @@ import { createServer } from '@graphql-yoga/node';
 
 import { schema } from './schema';
 
-const armor = new EnvelopArmor({
-  characterLimit: {
-    enabled: true,
-    maxLength: 2000,
-  },
-  costLimit: {
-    enabled: true,
-    maxCost: 100,
-    objectCost: 1,
-    scalarCost: 1,
-    depthCostFactor: 2,
-    ignoreIntrospection: true,
-  },
-  maxAliases: {
-    enabled: true,
-    n: 1,
-  },
-  maxDirectives: {
-    n: 10,
-  },
-  maxDepth: {
-    n: 4,
-  },
-});
-const enhancements = armor.protect();
-
 export function initServer() {
   const server = createServer({
     schema,
-    plugins: [EnvelopArmorPlugin()],
+    plugins: [
+      EnvelopArmorPlugin({
+        characterLimit: {
+          enabled: true,
+          maxLength: 2000,
+        },
+        costLimit: {
+          enabled: true,
+          maxCost: 100,
+          objectCost: 1,
+          scalarCost: 1,
+          depthCostFactor: 2,
+          ignoreIntrospection: true,
+        },
+        maxAliases: {
+          enabled: true,
+          n: 1,
+        },
+        maxDirectives: {
+          n: 10,
+        },
+        maxDepth: {
+          n: 4,
+        },
+      }),
+    ],
   });
   return server;
 }

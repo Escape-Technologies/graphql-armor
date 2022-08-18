e859bb8c897401e29a514ca8d9315057fec1686f
c3b5aw <pro@c3b5aw.dev>
2022-08-18 12:04:56 +0200

    feat(example/yoga): test introspection

--- examples/yoga/test/index.spec.ts
+++ examples/yoga/test/index.spec.ts
@@ -1,4 +1,6 @@
 import { describe, expect, it } from '@jest/globals';
+import { getIntrospectionQuery } from 'graphql';
+import request from 'supertest';
 
 import { initServer } from '../src/server';
 
@@ -6,5 +8,17 @@ describe('startup', () => {
   const server = initServer();
   it('should configure', () => {
     expect(server).toBeDefined();
+  });
+
+  it('should allow introspection', async () => {
+    const response = await request(server).post('/graphql').send({
+      query: getIntrospectionQuery(),
+    });
+
+    expect(response.statusCode).toBe(200);
+    const body = JSON.parse(response.text);
+    expect(body.data).toBeDefined();
+    expect(body.data?.__schema).toBeDefined();
+    expect(body.errors).toBeUndefined();
   });
 });

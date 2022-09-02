0dd9f59caa0b23d583bbe746550e13491a919d42
Laurin Quast <laurinquast@googlemail.com>
2022-09-02 15:57:18 +0200

    feat: document-token-limit

---
+++ packages/plugins/document-token-limit/README.md
@@ -0,0 +1,39 @@
+# @escape.tech/graphql-armor-document-token-limit
+
+This plugin will limit the number of tokens in a GraphQL operation.
+
+## Getting Started
+
+```bash
+# npm
+npm install @escape.tech/graphql-armor-document-token-limit
+
+# yarn
+yarn add @escape.tech/graphql-armor-document-token-limit
+```
+
+## Usage example
+
+### With `@envelop/core` from `@the-guild-org`
+
+```typescript
+import { envelop } from '@envelop/core';
+import { documentTokenLimitPlugin } from '@escape.tech/graphql-armor-document-token-limit';
+
+const getEnveloped = envelop({
+  plugins: [
+    // ... other plugins ...
+    documentTokenLimitPlugin({
+        maxTokenCount: 2000, // Number of characters allowed | Default: 2000
+    }),
+  ]
+})
+```
+
+## Design considerations
+
+This plugin relies on a parser plugin to works and access query from the context.
+
+This should be supported by the engine you use.
+
+If you experience any issues, please open an issue.
---
+++ packages/plugins/document-token-limit/test/index.spec.ts
@@ -0,0 +1,56 @@
+import { assertSingleExecutionValue, createTestkit } from '@envelop/testing';
+import { describe, expect, it } from '@jest/globals';
+import { buildSchema } from 'graphql';
+
+import { documentTokenLimitDefaultOptions, documentTokenLimitPlugin } from '../src/index';
+
+const schema = buildSchema(/* GraphQL */ `
+  type Query {
+    a: String
+  }
+`);
+
+describe('global', () => {
+  it('should be defined', () => {
+    expect(documentTokenLimitPlugin).toBeDefined();
+
+    documentTokenLimitPlugin();
+    documentTokenLimitPlugin({});
+    documentTokenLimitPlugin({ maxTokenCount: 1 });
+  });
+
+  it('rejects an operation with more than the default max token count', async () => {
+    const operation = `{ ${Array(documentTokenLimitDefaultOptions.maxTokenCount).join('a ')} }`;
+    const testkit = createTestkit([documentTokenLimitPlugin()], schema);
+    const result = await testkit.execute(operation);
+    assertSingleExecutionValue(result);
+    expect(result.errors).toBeDefined();
+    expect(result.errors).toHaveLength(1);
+    expect(result.errors?.[0].message).toEqual('Syntax Error: Token limit of 1000 exceeded.');
+  });
+  it('does not rejects an oepration below the max token count', async () => {
+    const operation = `{ ${Array(documentTokenLimitDefaultOptions.maxTokenCount - 2).join('a ')} }`;
+    const testkit = createTestkit([documentTokenLimitPlugin()], schema);
+    const result = await testkit.execute(operation);
+    assertSingleExecutionValue(result);
+    expect(result.errors).toBeUndefined();
+  });
+  it('rejects an operation with more than the default max token count (user provided)', async () => {
+    const count = 4;
+    const operation = `{ ${Array(count).join('a ')} }`;
+    const testkit = createTestkit([documentTokenLimitPlugin({ maxTokenCount: 4 })], schema);
+    const result = await testkit.execute(operation);
+    assertSingleExecutionValue(result);
+    expect(result.errors).toBeDefined();
+    expect(result.errors).toHaveLength(1);
+    expect(result.errors?.[0].message).toEqual('Syntax Error: Token limit of 4 exceeded.');
+  });
+  it('does not rejects an oepration below the max token count (user provided)', async () => {
+    const count = 4;
+    const operation = `{ ${Array(count - 2).join('a ')} }`;
+    const testkit = createTestkit([documentTokenLimitPlugin({ maxTokenCount: 4 })], schema);
+    const result = await testkit.execute(operation);
+    assertSingleExecutionValue(result);
+    expect(result.errors).toBeUndefined();
+  });
+});

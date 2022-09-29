dcdcda357cb15d2f68145161080e582d0b093dc2
c3b5aw <pro@c3b5aw.dev>
2022-09-29 15:43:38 +0200

    feat(docs): config & scripts

---
+++ packages/docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress';
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'GraphQL Armor Documentation',
+  description:
+    'Documentation of GraphQL Armor, The missing GraphQL security security layer for GraphQL Node ecosystem.',
+});

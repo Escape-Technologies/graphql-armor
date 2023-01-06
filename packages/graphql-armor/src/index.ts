cf8203949e07a33dbff604a99f22d039ce5e7535
David Thyresson <dthyresson@gmail.com>
2023-01-06 11:37:29 -0500

    fix: exports GraphQLArmorConfig type globally

--- packages/graphql-armor/src/index.ts
+++ packages/graphql-armor/src/index.ts
@@ -1,2 +1,4 @@
+export type { GraphQLArmorConfig } from './config';
+
 export { ApolloArmor } from './apollo/armor';
 export { EnvelopArmor, EnvelopArmorPlugin } from './envelop/armor';
--- packages/graphql-armor/test/index.spec.ts
+++ packages/graphql-armor/test/index.spec.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from '@jest/globals';
 
+import type { GraphQLArmorConfig } from '../src/index';
 import { ApolloArmor, EnvelopArmor } from '../src/index';
 
 describe('global', () => {
@@ -14,5 +15,12 @@ describe('global', () => {
 
     const envelop = new EnvelopArmor();
     expect(envelop).toBeDefined();
+  });
+
+  it('should export the GraphQLArmorConfig globally, so can set custom typed configuration', () => {
+    const config: GraphQLArmorConfig = { maxDepth: { enabled: false, n: 1 } };
+
+    expect(config).toBeDefined();
+    expect(config.maxDepth?.enabled).toBeFalsy();
   });
 });

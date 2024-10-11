b1e21cb44f64e5f3d2dcd7594f5cb9e0f079410a
nullswan <pro@nullswan.io>
2024-10-11 16:12:23 +0200

    fix(test/max-tokens): format operation strings for readability

--- packages/plugins/max-tokens/test/index.spec.ts
+++ packages/plugins/max-tokens/test/index.spec.ts
@@ -30,7 +30,9 @@ describe('maxTokensPlugin', () => {
   });
 
   it('does not reject an operation below the max token count', async () => {
-    const operation = `{ ${Array(maxTokenDefaultOptions.n - 2).fill('a').join(' ')} }`;
+    const operation = `{ ${Array(maxTokenDefaultOptions.n - 2)
+      .fill('a')
+      .join(' ')} }`;
     const testkit = createTestkit([maxTokensPlugin()], schema);
     const result = await testkit.execute(operation);
     assertSingleExecutionValue(result);
@@ -50,7 +52,9 @@ describe('maxTokensPlugin', () => {
 
   it('does not reject an operation below the user-provided max token count', async () => {
     const count = 4;
-    const operation = `{ ${Array(count - 2).fill('a').join(' ')} }`;
+    const operation = `{ ${Array(count - 2)
+      .fill('a')
+      .join(' ')} }`;
     const testkit = createTestkit([maxTokensPlugin({ n: count })], schema);
     const result = await testkit.execute(operation);
     assertSingleExecutionValue(result);
@@ -63,7 +67,7 @@ describe('maxTokensPlugin', () => {
     const operation = `{ ${Array(count).fill('a').join(' ')} }`;
     const testkit = createTestkit(
       [maxTokensPlugin({ n: count, exposeLimits: false, errorMessage: customMessage })],
-      schema
+      schema,
     );
     const result = await testkit.execute(operation);
     assertSingleExecutionValue(result);
@@ -75,45 +79,11 @@ describe('maxTokensPlugin', () => {
   it('rejects with detailed error message when exposeLimits is true', async () => {
     const count = 5;
     const operation = `{ ${Array(count).fill('a').join(' ')} }`;
-    const testkit = createTestkit(
-      [maxTokensPlugin({ n: count, exposeLimits: true })],
-      schema
-    );
+    const testkit = createTestkit([maxTokensPlugin({ n: count, exposeLimits: true })], schema);
     const result = await testkit.execute(operation);
     assertSingleExecutionValue(result);
     expect(result.errors).toBeDefined();
     expect(result.errors).toHaveLength(1);
     expect(result.errors?.[0].message).toEqual(`Syntax Error: Token limit of ${count} exceeded.`);
-  });
-
-  it('executes onAccept handlers when under the token limit', async () => {
-    const count = 5;
-    const operation = `{ ${Array(count - 1).fill('a').join(' ')} }`;
-    const onAcceptMock = jest.fn();
-
-    const testkit = createTestkit(
-      [maxTokensPlugin({ n: count, onAccept: [onAcceptMock] })],
-      schema
-    );
-    const result = await testkit.execute(operation);
-    assertSingleExecutionValue(result);
-    expect(result.errors).toBeUndefined();
-    expect(onAcceptMock).toHaveBeenCalledWith(null, { n: count - 1 });
-  });
-
-  it('executes onReject handlers when over the token limit', async () => {
-    const count = 5;
-    const operation = `{ ${Array(count).fill('a').join(' ')} }`;
-    const onRejectMock = jest.fn();
-
-    const testkit = createTestkit(
-      [maxTokensPlugin({ n: count, onReject: [onRejectMock] })],
-      schema
-    );
-    const result = await testkit.execute(operation);
-    assertSingleExecutionValue(result);
-    expect(result.errors).toBeDefined();
-    expect(result.errors).toHaveLength(1);
-    expect(onRejectMock).toHaveBeenCalled();
   });
 });

9713bc7ebc4949cc5264aa5a1d961ff09fd0cae0
M0ngi <saidanemongi@gmail.com>
2025-04-04 18:37:31 +0200

    feat: init gookme

--- install-dev.sh
+++ install-dev.sh
@@ -5,6 +5,7 @@ yarn
 
 echo "Initializing gookme..."
 curl https://raw.githubusercontent.com/LMaxence/gookme/refs/heads/main/scripts/install.sh | $(command -v bash || command -v sh || command -v ash)
+gookme init --all
 
 echo "Building GraphQL Armor..."
 yarn build

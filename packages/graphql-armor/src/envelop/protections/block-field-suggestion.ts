41f4314658d3b8012f495be312e75cde5c9b1748
Denis Badurina <denis@denelop.com>
2025-05-21 15:04:23 +0200

    armor

--- packages/graphql-armor/src/envelop/protections/block-field-suggestion.ts
+++ packages/graphql-armor/src/envelop/protections/block-field-suggestion.ts
@@ -2,7 +2,9 @@ import { blockFieldSuggestionsPlugin } from '@escape.tech/graphql-armor-block-fi
 
 import { EnvelopConfigurationEnhancement, EnvelopProtection } from './base-protection';
 
-export class EnvelopBlockFieldSuggestionProtection extends EnvelopProtection {
+export class EnvelopBlockFieldSuggestionProtection<
+  PluginContext extends Record<string, any> = {},
+> extends EnvelopProtection<PluginContext> {
   get isEnabled(): boolean {
     if (!this.config.blockFieldSuggestion) {
       return this.enabledByDefault;
@@ -10,7 +12,7 @@ export class EnvelopBlockFieldSuggestionProtection extends EnvelopProtection {
     return this.config.blockFieldSuggestion.enabled ?? this.enabledByDefault;
   }
 
-  protect(): EnvelopConfigurationEnhancement {
+  protect(): EnvelopConfigurationEnhancement<PluginContext> {
     return {
       plugins: [blockFieldSuggestionsPlugin(this.config.blockFieldSuggestion)],
     };
